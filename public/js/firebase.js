8f4e35200033d76a3f1ffa8ecad4a3c14047b6d9
watakook <ookami69wataipad@gmail.com>
2024-10-08 19:55:11 -0700

    second commit

---
+++ public/js/firebase.js
@@ -0,0 +1,24 @@
+/*
+  CCTB Website Development
+  IST105
+  Oct 2024
+  Description: This is a simple login website where students are asked to 
+  implement Social Network Login with Firebase
+*/
+
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDwL4_FbfY-HVRM-QOukN8m-68fqvqXt6A",
+  authDomain: "portfoliov1wataruokada.firebaseapp.com",
+  projectId: "portfoliov1wataruokada",
+  storageBucket: "portfoliov1wataruokada.appspot.com",
+  messagingSenderId: "409334819695",
+  appId: "1:409334819695:web:2aced72ff0d67d5e49d69a"
+};
+
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+
---
+++ public/js/main.js
@@ -0,0 +1,75 @@
+/*
+  CCTB Website Development
+  IST105
+  Oct 2024
+  Description: This is a simple login website where students are asked to 
+  implement Social Network Login with Firebase
+*/
+
+/*
+Function onAuthStateChanged(user)
+Write a function to check if a user is logged
+*/
+
+function authStateListener() {
+    // [START auth_state_listener]
+    firebase.auth().onAuthStateChanged((user) => {
+        if (user) {
+            // User is signed in, see docs for a list of available properties
+            // https://firebase.google.com/docs/reference/js/v8/firebase.User
+            var uid = user.uid;
+            // ...
+            location.href = 'listprojects.html';
+        } else {
+            // User is signed out
+            // ...
+
+        }
+    });
+    // [END auth_state_listener]
+}
+
+window.addEventListener('load', function () {
+
+
+    //Listen for auth state changes
+    authStateListener();
+
+    document.getElementById('sign-in-button').addEventListener('click', function () {
+
+        let provider = new firebase.auth.GoogleAuthProvider();
+
+        provider.addScope('email');
+        firebase.auth().signInWithPopup(provider)
+            .then(function (result) {
+                console.log('Logging sucessfully', result.user);
+                location.href = 'listprojects.html';
+            })
+            .catch(function (error) {
+                console.log('Logging fail', error);
+            });
+    });
+
+    document.getElementById('sign-in-2').addEventListener('click', function () {
+
+        let emailTxt = document.getElementById('email').value;
+        let passtxt = document.getElementById('password').value;
+
+        firebase.auth().signInWithEmailAndPassword(emailTxt, passtxt)
+            .then((userCredential) => {
+                // Signed in
+                let user = userCredential.user;
+                // ...
+                console.log('Logging sucessfully');
+                alert('Logging sucessfully');
+                location.href = 'listprojects.html';
+            })
+            .catch((error) => {
+                let errorCode = error.code;
+                let errorMessage = error.message;
+                alert('Logging fail');
+                console.log('Logging fail', errorMessage);
+            });
+
+    });
+});
